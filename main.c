46e6d16a8e6b0099fcace5378f1bb88b6806a3df
sumopoops <11431120+sumopoops@users.noreply.github.com>
2022-07-25 23:55:07 -0700

    Fixed grid not having stored values

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <stdio.h>
 
 //---------------------------------------------------------------------------------------- STRUCTS
 
@@ -10,7 +11,7 @@
 
 const int screenWidth = 60;
 const int screenHeight = 60;
-int grid[10][10];
+int grid[6][5];
 Texture2D textures[10];
 
 
@@ -18,20 +19,20 @@ Texture2D textures[10];
 //---------------------------------------------------------------------------------------- FUNCTIONS
 
 void TempPopulateGrid() {
-	for (int a=0; a<10; a++) {
-		for (int b=0; b<10; b++) {
+	for (int a=0; a<6; a++) {
+		for (int b=0; b<5; b++) {
 			grid[a][b] = 1;
 		}
 	}
-	grid[1][2] = 1;
 }
 
 void DrawIconGrid() {
-	for (int a=0; a<10; a++)
-	for (int b=0; b<10; b++) {
-		//if (!grid[a][b]) break;
-		DrawTexture(textures[grid[b][a]], a*10, b*10, WHITE);
-		printf("Grid Value: %d\n\n", grid[a][b]);
+	for (int a=0; a<6; a++) {
+		for (int b=0; b<5; b++) {
+			//if (!grid[a][b]) break;
+			DrawTexture(textures[grid[a][b]], a*10, b*10, WHITE);
+			//printf("Grid Value: %d\n\n", grid[a][b]);
+		}
 	}
 }
 
@@ -42,11 +43,10 @@ void DrawIconGrid() {
 int main() {
 
 	// Init Window Stuff
-	const char windowed = 0; // Make 0 for Fullscreen
+	const char windowed = 10; // Make 0 for Fullscreen
 	float scale;
 	float playAreaX;
 	SetConfigFlags(FLAG_VSYNC_HINT);
-
 	if (windowed) {
 		InitWindow(screenWidth*windowed, screenHeight*windowed, "Invade");
 		scale = (float)windowed;
@@ -61,9 +61,15 @@ int main() {
 
 	// Load assets
 	textures[1] = LoadTexture("img/1.png");
+	Texture2D TX_sprites = LoadTexture("img/sprites.png");
+	Texture2D TX_logo = LoadTexture("img/logo.png");
+	Texture2D TX_hud = LoadTexture("img/hud.png");
 
 	RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
     SetTargetFPS(60);
+
+	// Init
+	TempPopulateGrid();
 
     while (!WindowShouldClose()) {
 
@@ -75,6 +81,7 @@ int main() {
 			// DRAW EVERYTHING HERE
 			ClearBackground((Color){33, 33, 33, 255});
 			DrawIconGrid();
+			DrawTexture(TX_hud, 0, 50, WHITE);
 		
 		EndTextureMode();
 
