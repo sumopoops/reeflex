857fa51c4af8f12d296ec7a4a805a5752224c6d9
sumopoops <11431120+sumopoops@users.noreply.github.com>
2022-11-23 15:45:13 -0800

    Cleanup and README

---
+++ README.md
@@ -0,0 +1,6 @@
+# REEFLX
+
+A short pixel art game that tests your reflexes, created using Raylib.
+
+[Download REEFLX on itch.io](https://sumopoops.itch.io/reeflx)
+
--- debug.bat
+++
@@ -1,2 +0,0 @@
-cls
-gcc main.c -g -o main.out -O1 -Wall -std=c99 -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows && gdb main.out
